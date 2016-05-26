4b132a9bae9c035347f7163e59cdd37003471012
tmoroz <teomoroz@gmail.com>
2016-05-26 12:38:02 +0300

    splitted test code into separate routines

--- main.cpp
+++ main.cpp
@@ -3,32 +3,42 @@
 #include "sort.h"
 #include "graph.h"
 
+void MinimumCutTest();
+void MinimumPathTest();
+void MergeSortTest();
+void QuickSortTest();
 
 int main(int argc, char *argv[])
 {
-	int test[] = { 1, 23,64, 5, 7, 2, 56, 77, 234, 23,5,6,8,9,34,6,6,546,};
+    MinimumCutTest();
 
-	std::vector<int> vVector(test, test + sizeof(test) / sizeof(int));
+	return 0;
+}
 
-    //int iSplit = Sort::MergeSort(vVector.begin(), vVector.end());
-    //std::cout << "Split inversions - " << iSplit << std::endl;
+void MergeSortTest()
+{
+    int test[] = { 1, 23,64, 5, 7, 2, 56, 77, 234, 23,5,6,8,9,34,6,6,546, };
+    std::vector<int> vVector(test, test + sizeof(test) / sizeof(int));
 
-    /*Sort::QuickSort(vVector.begin(), vVector.end());
+    int iSplit = Sort::MergeSort(vVector.begin(), vVector.end());
+    std::cout << "Split inversions - " << iSplit << std::endl;
 
-	Algo::PrintContainer(vVector.begin(), vVector.end());
-*/
+    Algo::PrintContainer(vVector.begin(), vVector.end());
+}
 
-	//TODO: v2012 does not support init list;
+void QuickSortTest()
+{
+    int test[] = { 1, 23,64, 5, 7, 2, 56, 77, 234, 23,5,6,8,9,34,6,6,546, };
+    std::vector<int> vVector(test, test + sizeof(test) / sizeof(int));
+    Sort::QuickSort(vVector.begin(), vVector.end());
+
+    Algo::PrintContainer(vVector.begin(), vVector.end());
+   
+}
+
+void MinimumPathTest()
+{
 #ifdef  _MSC_PLATFORM_TOOLSET_v140
-    //Graph graph({ { 0, 1, 0, 0, 1 },
-    //              { 1, 0, 1, 0, 1 },
-    //              { 0, 1, 0, 1, 1 },
-    //              { 0, 0, 1, 0, 1 },
-    //              { 1, 1, 1, 1, 0} });
-    //auto minimumCut = graph.FindMinimumCut();
-    //size_t iMinimumCut = minimumCut.size();
-    //std::cout << iMinimumCut;
-
     Graph graphPath({ {0, 1, 1, 0, 0, 0},
                     {1, 0, 0, 1, 0, 0},
                     {1, 0, 0, 1, 1, 0 },
@@ -37,18 +47,29 @@ int main(int argc, char *argv[])
                     {0, 0, 0, 1, 1, 0 }, });
     int iPath = graphPath.FindMinimumPath(2, 4);
     std::cout << iPath;
+#else
+#endif
+}
 
+void MinimumCutTest()
+{
+#ifdef  _MSC_PLATFORM_TOOLSET_v140
+    Graph graph({ { 0, 1, 0, 0, 1 },
+    { 1, 0, 1, 0, 1 },
+    { 0, 1, 0, 1, 1 },
+    { 0, 0, 1, 0, 1 },
+    { 1, 1, 1, 1, 0 } });
+    auto minimumCut = graph.FindMinimumCut();
+    size_t iMinimumCut = minimumCut.size();
+    std::cout << iMinimumCut;
 #else
     int line1[] = { 0, 1, 0 };
     int line2[] = { 1, 0, 1 };
     int line3[] = { 0, 1, 0 };
-/*      { 0, 1, 0 },
-        { 1, 0, 1 },
-        { 0, 1, 0 }
-*/
+    /*      { 0, 1, 0 },
+    { 1, 0, 1 },
+    { 0, 1, 0 }
+    */
     //TODO: support initializer list
-    
 #endif
-
-	return 0;
 }
